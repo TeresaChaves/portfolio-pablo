6e91ce9505201a88e0a901abc9915a08e1929f1f
TeresaChaves <113774017+TeresaChaves@users.noreply.github.com>
2024-10-06 11:23:01 +0200

    responsive proyectos

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Proyectos.css";
 import uno from "../assets/uno.jpeg";
 import dos from "../assets/dos.jpeg";
@@ -9,156 +9,132 @@ import ocho from "../assets/prueba/A marcela.jpg";
 import { NavLink } from "react-router-dom";
 
 function Proyectos() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 1200);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1200);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Array de proyectos
+  const proyectos = [
+    {
+      title: "Cola de pez",
+      imgSrc: uno,
+      direccion: "Natalia Huerte y Javier Lara",
+      lugar: "Bastero Kulturgunea",
+      link: "/cola-de-pez",
+    },
+    {
+      title: "No estoy de frente",
+      imgSrc: dos,
+      direccion: "Paco León",
+      lugar: "Sala Exlímite",
+      link: "/no-estoy-de-frente",
+    },
+    {
+      title: "Cucharacha con paisaje de fondo",
+      imgSrc: cinco,
+      direccion: "Javier Ballesteros",
+      lugar: "Teatro Español",
+      link: "/cucaracha",
+    },
+    {
+      title: "El banquete",
+      imgSrc: siete,
+      direccion: "Adrián Pulido",
+      lugar: "Nave 73",
+      link: "/elbanquete",
+    },
+    {
+      title: "Gaviotas",
+      imgSrc: seis,
+      direccion: "Gritadero Teatro",
+      lugar: "Umbral del la Primavera",
+      link: "/gaviotas",
+    },
+    {
+      title: "Marcela: Una canción de Cervantes",
+      imgSrc: ocho,
+      direccion: "Leticia Dolera",
+      lugar: "Sala Cervantina",
+      link: "/marcela",
+    },
+  ];
+
   return (
     <section className="home-margin">
       <header className="header-display">
         <p>proyectos</p>
       </header>
-      <NavLink to="/cola-de-pez">
-        <article className="article-proyectos-display">
-          <div className="texto-proyectos-name">
-            <p>Cola de pez</p>
-          </div>
-          <div>
-            <img
-              className="img-proyectos"
-              src={uno}
-              alt="foto de proyecto uno"
-            ></img>
-          </div>
-          <div className="texto-proyectos-name">
-            <div className="entrelineas">
-              <span className="text-teatro-light">Dirección: </span>
-              <span>Natalia Huerte y Javier Lara</span>
-            </div>
-            <div>
-              <span className="text-teatro-light">En: </span>
-              <span>Bastero Kulturgunea</span>
-            </div>
-          </div>
-        </article>
-      </NavLink>
-      <NavLink to="/no-estoy-de-frente">
-        <article className="article-proyectos-display">
-          <div className="texto-proyectos-name">
-            <p>No estoy de frente</p>
-          </div>
-          <div>
-            <img
-              className="img-proyectos"
-              src={dos}
-              alt="foto de proyecto dos"
-            ></img>
-          </div>
-          <div className="texto-proyectos-name">
-            <div className="entrelineas">
-              <span className="text-teatro-light">Dirección: </span>
-              <span>Paco León</span>
-            </div>
-            <div>
-              <span className="text-teatro-light">En: </span>
-              <span>Sala Exlímite</span>
-            </div>
-          </div>
-        </article>
-      </NavLink>
-      <NavLink to="/cucaracha">
-        <article className="article-proyectos-display">
-          <div className="texto-proyectos-name">
-            <p>Cucharacha con paisaje de fondo</p>
-          </div>
-          <div>
-            <img
-              className="img-proyectos"
-              src={cinco}
-              alt="foto de proyecto cinco"
-            ></img>
-          </div>
-          <div className="texto-proyectos-name">
-            <div className="entrelineas">
-              <span className="text-teatro-light">Dirección: </span>
-              <span>Javier Ballesteros</span>
-            </div>
-            <div>
-              <span className="text-teatro-light">En: </span>
-              <span>Teatro Español</span>
-            </div>
-          </div>
-        </article>{" "}
-      </NavLink>
-      <NavLink to="/elbanquete">
-        <article className="article-proyectos-display">
-          <div className="texto-proyectos-name">
-            <p>El banquete</p>
-          </div>
-          <div>
-            <img
-              className="img-proyectos"
-              src={siete}
-              alt="foto de proyecto siete"
-            ></img>
-          </div>
-          <div className="texto-proyectos-name">
-            <div className="entrelineas">
-              <span className="text-teatro-light">Dirección: </span>
-              <span>Adrián Pulido</span>
-            </div>
-            <div>
-              <span className="text-teatro-light">En: </span>
-              <span>Nave 73</span>
-            </div>
-          </div>
-        </article>
-      </NavLink>
-      <NavLink to="/gaviotas">
-        <article className="article-proyectos-display">
-          <div className="texto-proyectos-name">
-            <p>Gaviotas</p>
-          </div>
-          <div>
-            <img
-              className="img-proyectos img-top"
-              src={seis}
-              alt="foto de proyecto seis"
-            ></img>
-          </div>
-          <div className="texto-proyectos-name">
-            <div className="entrelineas">
-              <span className="text-teatro-light">Dirección: </span>
-              <span>Gritadero Teatro</span>
-            </div>
-            <div>
-              <span className="text-teatro-light">En: </span>
-              <span>Umbral del la Primavera</span>
-            </div>
-          </div>
-        </article>
-      </NavLink>
-      <NavLink to="/marcela">
-        <article className="article-proyectos-display">
-          <div className="texto-proyectos-name">
-            <p>Marcela: Una canción de Cervantes</p>
-          </div>
-          <div>
-            <img
-              className="img-proyectos img-top"
-              src={ocho}
-              alt="foto de proyecto seis"
-            ></img>
-          </div>
-          <div className="texto-proyectos-name">
-            <div className="entrelineas">
-              <span className="text-teatro-light">Dirección: </span>
-              <span>Leticia Dolera</span>
-            </div>
-            <div>
-              <span className="text-teatro-light">En: </span>
-              <span>Sala Cervantina</span>
-            </div>
-          </div>
-        </article>
-      </NavLink>
+
+      {proyectos.map((proyecto) => (
+        <NavLink to={proyecto.link} key={proyecto.title}>
+
+          <article className="article-proyectos-display">
+            {/* Condicional para la disposición de texto e imagen */}
+            {isMobile ? (
+              <>
+                <div className="container-img-proyectos">
+                  <img
+                    className="img-proyectos"
+                    src={proyecto.imgSrc}
+                    alt={`foto de proyecto ${proyecto.title}`}
+                  />
+                <div class="responsive-align-project-name">
+                <div className="texto-proyectos-name">
+                  <p>{proyecto.title}</p>
+                </div>
+
+                  <div className="texto-proyectos-name">
+                  <div className="entrelineas">
+                    <span className="text-teatro-light">Dirección: </span>
+                    <span>{proyecto.direccion}</span>
+                  </div>
+                  <div>
+                    <span className="text-teatro-light">En: </span>
+                    <span>{proyecto.lugar}</span>
+                  </div>
+                  </div>
+                </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="texto-proyectos-name">
+                  <p>{proyecto.title}</p>
+                </div>
+                <div className="container-img-proyectos">
+                  <img
+                    className="img-proyectos"
+                    src={proyecto.imgSrc}
+                    alt={`foto de proyecto ${proyecto.title}`}
+                  />
+                </div>
+                <div className="texto-proyectos-name">
+                  <div className="entrelineas">
+                    <span className="text-teatro-light">Dirección: </span>
+                    <span>{proyecto.direccion}</span>
+                  </div>
+                  <div>
+                    <span className="text-teatro-light">En: </span>
+                    <span>{proyecto.lugar}</span>
+                  </div>
+                </div>
+              </>
+            )}
+          </article>
+        </NavLink>
+      ))}
     </section>
   );
 }
+
 export default Proyectos;
